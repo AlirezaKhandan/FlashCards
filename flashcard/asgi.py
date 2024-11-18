ea51c8bf4e4cc90d69d4bc8fb3a6d98f0c617531
AlirezaKhandan <124064697+AlirezaKhandan@users.noreply.github.com>
2024-11-18 12:36:05 +0000

    changed a few stuff and changed the name to flashcard

--- flashcard/asgi.py
+++ flashcard/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for hello_world project.
+ASGI config for flashcard project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -18,6 +18,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hello_world.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "flashcard.settings")
 
 application = get_asgi_application()
